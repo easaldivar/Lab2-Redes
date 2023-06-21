4440723092ae83e5c00e24e973e130a0227ec759
Ana Vargas <avargas2020@inf.udec.cl>
2023-06-21 18:55:45 -0400

    Servidor y codigos de respuesta funcionan

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Lab 2: Servidor HTTP simple
 ---
+
+Para ejecutar el código se escribe en la terminal: "node server.js"
+
+Luego en el buscador de google para buscar la página se escribe: "http://localhost:8080"
+
+Para verificar que los códigos de respuesta funcionan correctamente en Powershell, primero que todo, hay que asegurarse de que se esté ejecutando la página y en otro terminal escribir los siguientes comandos para cada caso:
+
+200 OK: Invoke-WebRequest -Uri "http://localhost:8080"
+301 Moved Permanently: Invoke-WebRequest -Uri "http://localhost:8080/moved" -MaximumRedirection 0
+400 Bad Request: Invoke-WebRequest -Uri "http://localhost:8080" -Method POST
+404 Not Found: Invoke-WebRequest -Uri "http://localhost:8080/unknown"
+405 Method Not Allowed: Invoke-WebRequest -Uri http://localhost:8080/ -Method POST
+505 HTTP Version Not Supported: Invoke-WebRequest -Uri "http://localhost:8080" -Method PUT
+
+
+
+
--- server.js
+++ server.js
@@ -1,19 +1,41 @@
-const net = require('net');
-const port = 7070;
+const http = require('http');
 
-const server = net.createServer((socket) => {
-    console.log('Cliente conectado');
+//Se crea el servidor
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
 
-    socket.on('data', (data) => {
-        console.log(data.toString());
-    });
+  //Se definen las respuestas del servidor 
+  if (method === 'GET') {
+    if (url === '/') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Hello, World!\n');
+    } else if (url === '/moved') {
+      res.statusCode = 301;
+      res.setHeader('Location', 'http://localhost:8080/');
+      res.end();
+    } else if (url === '/notfound') {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not Found\n');
+    } else {
+      res.statusCode = 405;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Method Not Allowed\n');
+    }
+  } else if (method === 'POST') {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Bad Request\n');
+  } else {
+    res.statusCode = 505;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('HTTP Version Not Supported\n');
+  }
+});
 
-    socket.on('end', () => {
-        console.log('Cliente desconectado');
-        socket.end();
-    });
-
-    socket.on('error', (err) => {
-        console.log(err);
-    });
+//Se define el puerto donde se ejecutará el servidor 
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
