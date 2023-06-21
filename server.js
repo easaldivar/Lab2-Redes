8b7830d94fce8fb9b4c55a2052dcb42a44f25ea3
Ana Vargas <avargas2020@inf.udec.cl>
2023-06-21 19:39:32 -0400

    Cambios

--- server.js
+++ server.js
@@ -1,41 +1,49 @@
-const http = require('http');
+const net = require('net');
 
-//Se crea el servidor
-const server = http.createServer((req, res) => {
-  const { method, url } = req;
+// Crea el servidor TCP
+const server = net.createServer((socket) => {
+  // Maneja la conexión entrante
 
-  //Se definen las respuestas del servidor 
-  if (method === 'GET') {
-    if (url === '/') {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Hello, World!\n');
-    } else if (url === '/moved') {
-      res.statusCode = 301;
-      res.setHeader('Location', 'http://localhost:8080/');
-      res.end();
-    } else if (url === '/notfound') {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Not Found\n');
+  // Registra el evento de datos recibidos
+  socket.on('data', (data) => {
+    const request = data.toString();
+
+    const [method, url] = parseRequest(request);
+
+    // Define la respuesta del servidor
+    let response = '';
+    if (method === 'GET') {
+      if (url === '/') {
+        response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!\r\n';
+      } else if (url === '/moved') {
+        response = 'HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:8080/\r\n\r\n';
+      } else if (url === '/notfound') {
+        response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nNot Found\r\n';
+      } else {
+        response = 'HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain\r\n\r\nMethod Not Allowed\r\n';
+      }
+    } else if (method === 'POST') {
+      response = 'HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nBad Request\r\n';
     } else {
-      res.statusCode = 405;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Method Not Allowed\n');
+      response = 'HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Type: text/plain\r\n\r\nHTTP Version Not Supported\r\n';
     }
-  } else if (method === 'POST') {
-    res.statusCode = 400;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Bad Request\n');
-  } else {
-    res.statusCode = 505;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('HTTP Version Not Supported\n');
-  }
+
+    // Envía la respuesta al cliente
+    socket.write(response);
+    socket.end();
+  });
 });
 
-//Se define el puerto donde se ejecutará el servidor 
+// Inicia el servidor en el puerto 8080
 const PORT = 8080;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+// Función para analizar la solicitud HTTP y extraer el método y la URL
+function parseRequest(request) {
+  const lines = request.split('\r\n');
+  const [method, url] = lines[0].split(' ');
+
+  return [method, url];
+}
