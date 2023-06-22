489fff45426a1b87a724e010e3c02cccf7269b77
Erick Saldivar <esaldivar2020@udec.cl>
2023-06-21 20:27:58 -0400

    server responde a request GET basica

--- server.js
+++ server.js
@@ -1,49 +1,70 @@
 const net = require('net');
+const port = 8080;
 
-// Crea el servidor TCP
 const server = net.createServer((socket) => {
-  // Maneja la conexión entrante
+    console.log('Cliente conectado');
 
-  // Registra el evento de datos recibidos
-  socket.on('data', (data) => {
-    const request = data.toString();
+    socket.on('data', (data) => {
+        console.log(data.toString());
+        const request = parseHTTPRequest(data.toString());
 
-    const [method, url] = parseRequest(request);
+        console.log(request);
 
-    // Define la respuesta del servidor
-    let response = '';
-    if (method === 'GET') {
-      if (url === '/') {
-        response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!\r\n';
-      } else if (url === '/moved') {
-        response = 'HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:8080/\r\n\r\n';
-      } else if (url === '/notfound') {
-        response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nNot Found\r\n';
-      } else {
-        response = 'HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain\r\n\r\nMethod Not Allowed\r\n';
+        if(request.path === '/'){
+            console.log('Peticion a /');
+            socket.write('HTTP/1.1 200 OK\r\nContent-Length: 12\r\nContent-Type: text/plain\r\n\r\nHello world!');
+        }
+    });
+
+    socket.on('end', () => {
+        console.log('Cliente desconectado');
+        socket.end();
+    });
+
+    socket.on('error', (err) => {
+        console.log(err);
+    });
+});
+server.listen(port, () => {
+    console.log("[SV] Servidor abierto en el puerto " + port);
+});
+
+
+// Parse HTTP request
+function parseHTTPRequest(request) {
+    const parsedRequest = {};
+  
+    // Divide la request en lineas
+    const lines = request.split('\r\n');
+  
+    // Extrae metodo, path y protocolo de la primera linea
+    const [method, path, protocol] = lines[0].split(' ');
+  
+    parsedRequest.method = method;
+    parsedRequest.path = path;
+    parsedRequest.protocol = protocol;
+  
+    // Headers
+    parsedRequest.headers = {};
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i];
+      if (line === '') {
+        break;
       }
-    } else if (method === 'POST') {
-      response = 'HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nBad Request\r\n';
-    } else {
-      response = 'HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Type: text/plain\r\n\r\nHTTP Version Not Supported\r\n';
+  
+      const [headerName, headerValue] = line.split(':');
+      const trimmedHeaderName = headerName.trim();
+      const trimmedHeaderValue = headerValue.trim();
+  
+      parsedRequest.headers[trimmedHeaderName] = trimmedHeaderValue;
     }
-
-    // Envía la respuesta al cliente
-    socket.write(response);
-    socket.end();
-  });
-});
-
-// Inicia el servidor en el puerto 8080
-const PORT = 8080;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
-
-// Función para analizar la solicitud HTTP y extraer el método y la URL
-function parseRequest(request) {
-  const lines = request.split('\r\n');
-  const [method, url] = lines[0].split(' ');
-
-  return [method, url];
+  
+    // Body
+    const requestBodyIndex = lines.indexOf('', 1);
+    if (requestBodyIndex !== -1 && requestBodyIndex < lines.length - 1) {
+      parsedRequest.body = lines.slice(requestBodyIndex + 1).join('\n');
+    }
+  
+    // Retorna objeto request
+    return parsedRequest;
 }
